d962bfc24ec45e0e6d346236c5abdb17c3a1680b
James Finlinson <finljam@byu.edu>
2024-09-04 13:30:59 -0600

    Implement main.cpp file

--- main.cpp
+++ main.cpp
@@ -6,10 +6,8 @@ int main() {
     auto rows = 25, cols = 50;
     auto c = 0.01;
     auto w = WaveOrthotope(rows, cols, c);
-    // Set interior cells of v to 0.1
-    // TODO
-    // Solve and print result
-    w.solve();
+    w.initInterior(0.0, 0.1);
+    w.solve(0.01);
     std::cout << w.sim_time() << std::endl;
     return 0;
 }
