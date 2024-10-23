27ebd526cad8c6c42c42eb55790b31629e3bd269
Ryan Weber <rweber32@login01.rc.byu.edu>
2024-10-22 19:54:33 -0600

    Phase 5 Complete

--- optimize.cpp
+++ optimize.cpp
@@ -1,10 +1,12 @@
+
 #include <iostream>
 #include <vector>
 #include <span>
 #include <cmath>
+#include <omp.h>
 
 
-
+const int &rows = 800;
 
 auto interior(auto &x) {
     return std::span(x.begin()+1, x.end()-1);
@@ -19,20 +21,20 @@ auto laplacian(const auto &x, auto i, auto j) {
 
 
 auto energy_floor(const auto &u) {
-    return (u.size() - 2) * (u.front().size() - 2) * 0.001;
+    return (rows - 2) * (rows - 2) * 0.001;
 }
 
 
 
 auto energy(const auto &u, const auto &v) {
     double E{};
-    auto m = u.size(), n = u.front().size();
+    auto m = rows, n = rows;
 
     // Dynamic
     #pragma omp parallel for reduction(+:E)
     for (auto row: interior(v)) {
         for (auto v_ij: interior(row)) {
-            E += std::pow(v_ij, 2) / 2;
+            E += (v_ij*v_ij) / 2;
         }
     }
 
@@ -41,14 +43,14 @@ auto energy(const auto &u, const auto &v) {
     for (size_t i=0; i<m-1; i++) {  
         for (size_t j=1; j<n-1; j++) {
             
-            E += std::pow(u[i][j]-u[i+1][j], 2) / 4;
+            E += ((u[i][j]-u[i+1][j])*(u[i][j]-u[i+1][j])) / 4;
         }
     }
     #pragma omp parallel for reduction(+:E)
     for (size_t i=1; i<m-1; i++) { 
         for (size_t j=0; j<n-1; j++) {
             
-            E += std::pow(u[i][j]-u[i][j+1], 2) / 4;
+            E += ((u[i][j]-u[i][j+1])*(u[i][j]-u[i][j+1])) / 4;
         }
     }
 
@@ -58,7 +60,7 @@ auto energy(const auto &u, const auto &v) {
 
 
 auto step(auto &u, auto &v, auto c, auto dt) {
-    auto m = u.size(), n = u.front().size();
+    auto m = rows, n = rows;
     double dtc = dt * c;
     // Update v
     #pragma omp parallel for
@@ -84,8 +86,8 @@ auto step(auto &u, auto &v, auto c, auto dt) {
 
 int main() {
     // Simulation parameters
-    const int rows = 800;
-    const double c = 0.05,
+    const int &rows = 800;
+    const double &c = 0.05,
                  dt = 0.01,
                  u0 = 1, v0 = 0;
     double t = 0;
@@ -93,10 +95,12 @@ int main() {
     // Initialize u and v
     auto u = std::vector<std::vector<double>>(rows, std::vector<double>(rows));
     auto v = u;
+    double e_f = energy_floor(u);
+    #pragma omp parallel for
     for (auto &row: interior(u)) std::fill(interior(row).begin(), interior(row).end(), u0);
-
+    
     // Solve
-    while (energy(u, v) > energy_floor(u)) {
+    while (energy(u, v) > e_f) {
         step(u, v, c, dt);
         t += dt;
     }
@@ -105,4 +109,5 @@ int main() {
     std::cout << t << std::endl;
     return 0;
 }
+
 
